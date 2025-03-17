b069c3c9b45f9c27bd630d82b45510b05c26876c
Kundz1st <doankun20051st@gmail.com>
2025-03-17 15:48:49 +0700

    Update bai2_4.py

--- Bài 2/bai2_4.py
+++ Bài 2/bai2_4.py
@@ -7,6 +7,6 @@ for j in range(2,10):
     print(f"số nghịch đảo của {j} là:",i,"/",j)
     #f hay f-string lệnh dùng để gắn biến vào xâu
     print("kết quả dưới dạng thập phân là",i/j)
-print("Sinh viên: Phạm Trọng Phúc")
-print(" MSSV: 235752020710029")
+print("Sinh viên: Lang Văn Doan")
+print(" MSSV: 235752020710023")
 # print: in ra màn hình
